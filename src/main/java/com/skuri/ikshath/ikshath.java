66c5db53c03af98a7ecfedd76584a0586e6aac8b
Doug Guzman <swiftshadow34@gmail.com>
2018-10-24 21:21:57 -0500

    adds basic functions

--- src/main/java/com/skuri/ikshath/ikshath.java
+++ src/main/java/com/skuri/ikshath/ikshath.java
@@ -1,4 +1,27 @@
 package com.skuri.ikshath;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ikshath.MODID, version = ikshath.VERSION)
 public class ikshath {
+
+    public static final String MODID = "ikshath";
+    public static final String VERSION = "0.01";
+    public static final String name = "Ikshath";
+
+
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    public void init (FMLInitializationEvent event) {
+
+    }
+    
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
 }
